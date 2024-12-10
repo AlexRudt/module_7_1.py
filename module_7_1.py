98d21ed40e10cd25d21124b512752abeb5f108f9
AlexRudt <Aleksrudt@yandex.ru>
2024-12-10 16:43:31 +0300

    Add files via upload

--- module_7_1.py
+++ module_7_1.py
@@ -1,34 +1,35 @@
-from pprint import pprint
 class Product:
     def __init__(self, name, weight, category):
         self.name = name
         self.weight = weight
         self.category = category
     def __str__(self):
-        return(f'{self.name}, {self.weight}, {self.category}')
+        return f'{self.name}, {self.weight}, {self.category}'
 
-class Shop(Product):
-    def __init__(self, name, weight, category, __file_name='products.txt'):
-        super().__init__(name, weight, category)
-        self.__file_name = __file_name
+class Shop():
+    def __init__(self):
+        self.__file_name = 'products.txt'
+
     def get_products(self):
-        file = open(self.__file_name, 'products.txt')
-        st = file.read()
+        file = open(self.__file_name, 'r')
+        products = file.read()
         file.close()
-        print(f'{st}')
+        return products
+
     def add(self, *products):
-        for i in products:
-            s = (str(i))
-            file = open(self.__file_name, 'products.txt')
-            f = file.read()
-            file.close()
-            if s in f:
-                print(f'Продукт {s} уже есть в магазине')
+        current_product = self.get_products()
+        file = open(self.__file_name, 'a')
+        for product in products:
+            if str(product) not in current_product:
+                file.write(str(product) + '\n')
+                current_product += str(product) + '\n'
             else:
-                file = open(self.__file_name, 'a')
-                file.write(f'\n{s}')
-                file.close()
-s1 = Shop('', 0, '')
+                print(f'Продукт {product} уже есть в магазине')
+        file.close()
+
+
+
+s1 = Shop()
 p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
